9eb14ca8861fc654a89ba46365205b42d9f6bf9a
iyazhi <134831948+iyazhi@users.noreply.github.com>
2024-10-08 10:19:34 +0530

    initial commit

---
+++ 02-react-build/customreact.js
@@ -0,0 +1,33 @@
+const root = document.querySelector('#root');
+
+const renderElement = {
+    type : 'a',
+    props : {
+        href : "http://www.google.com",
+        target : "_blank"
+    },
+    child :"click me"
+}
+
+function createElement(renderElement,root){
+    /*
+    const element = document.createElement(renderElement.type);
+    element.innerHTML = renderElement.child;
+    element.setAttribute('href',renderElement.prop['href']);
+    element.setAttribute('target',renderElement.prop['target']);
+    //
+    root.appendChild(element);
+    */
+
+    const element = document.createElement('a',renderElement.type);
+    element.innerHTML = renderElement.child;
+    for (const prop in renderElement.props) {
+        if (prop === 'children') continue;
+            element.setAttribute('prop',renderElement.props[prop]);   
+        }
+    root.appendChild(element);
+    }
+
+
+
+createElement(renderElement,root);
